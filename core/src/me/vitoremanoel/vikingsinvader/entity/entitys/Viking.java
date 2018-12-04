b2b3969a52abbbdb707372d54de16f324724d3ac
vitoremanoel <vitore88@gmail.com>
2018-12-04 13:36:19 -0200

    Adicionado a classe Viking

---
+++ core/src/me/vitoremanoel/vikingsinvader/entity/entitys/Viking.java
@@ -0,0 +1,16 @@
+package me.vitoremanoel.vikingsinvader.entity.entitys;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import me.vitoremanoel.vikingsinvader.animation.VikingAnimation;
+import me.vitoremanoel.vikingsinvader.animation.animations.AbstractAnimation;
+import me.vitoremanoel.vikingsinvader.animation.animations.AnimationIdle;
+import me.vitoremanoel.vikingsinvader.entity.entitys.Entity;
+
+
+public class Viking extends Entity {
+
+}
