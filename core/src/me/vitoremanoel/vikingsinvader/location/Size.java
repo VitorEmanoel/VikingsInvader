5e457ea252ae0ea7c3d1e1d8f74071ae5042d0c5
Vitor Emanoel Lacerda Amorim <vitore88@gmail.com>
2018-12-15 21:59:55 -0200

    - Arquivo para armazenar o tamanho dos itens

---
+++ core/src/me/vitoremanoel/vikingsinvader/location/Size.java
@@ -0,0 +1,27 @@
+package me.vitoremanoel.vikingsinvader.location;
+
+public class Size {
+
+    private float width, height;
+
+    public Size(float width, float height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
